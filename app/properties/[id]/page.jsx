4618e4caac2ce5e581d6b643078c04b9395534da
Kristjan Lääne <xonproject@gmail.com>
2024-03-06 19:03:54 +0200

    Server Components, Client Components & Router Hook

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+'use client';
+import {
+  useRouter,
+  useParams,
+  useSearchParams,
+  usePathname,
+} from 'next/navigation';
 
 const PropertyPage = () => {
-  return <div>PropertyPage</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const { id } = useParams();
+  const pathname = usePathname();
+
+  const name = searchParams.get('name');
+
+  return (
+    <div>
+      <button onClick={() => router.push('/')} className='bg-blue-500 p-2'>
+        Go Home {pathname}
+      </button>
+    </div>
+  );
 };
 
 export default PropertyPage;
