fec636822f07a12d054df2c845b7aface91e3a41
Kristjan Lääne <xonproject@gmail.com>
2024-03-14 16:31:23 +0200

    Fetch Data From Server Components

---
+++ utils/requests.js
@@ -0,0 +1,23 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
+
+async function fetchProperties() {
+  try {
+    // handkle case where domain is not available yet
+    if (!apiDomain) {
+      return [];
+    }
+
+    const res = await fetch(`${apiDomain}/properties`);
+
+    if (!res.ok) {
+      throw new Error('failed to fetch data');
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+export { fetchProperties };
