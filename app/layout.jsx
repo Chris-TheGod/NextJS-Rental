758d18a0491e9a560209f515c75e5350f60f4603
Kristjan Lääne <xonproject@gmail.com>
2024-03-17 17:13:41 +0200

    Session Provider Wrapper

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '@/assets/styles/globals.css';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
+import AuthProvider from '@/components/AuthProvider';
 
 export const metadata = {
   title: 'NextJS-Rental | Find The Perfect Renatal',
@@ -10,13 +11,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang='en'>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang='en'>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
