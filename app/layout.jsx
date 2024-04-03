3f264de46624642f04b882841469d1a69a96eb8b
Kristjan Lääne <xonproject@gmail.com>
2024-04-03 17:41:20 +0300

    Global Context For Unread Messages

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import Navbar from '@/components/Navbar';
 import { ToastContainer } from 'react-toastify';
 import AuthProvider from '@/components/AuthProvider';
 import 'react-toastify/dist/ReactToastify.css';
+import { GlobalProvider } from '@/context/GlobalContext';
 
 export const metadata = {
   title: 'NextJS-Rental | Find The Perfect Renatal',
@@ -13,16 +14,18 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <AuthProvider>
-      <html lang='en'>
-        <body>
-          <Navbar />
-          <main>{children}</main>
-          <Footer />
-          <ToastContainer />
-        </body>
-      </html>
-    </AuthProvider>
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang='en'>
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
   );
 };
 
