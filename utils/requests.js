826e0dc137741763bbccfa65ff1b7f090a6d14cc
Kristjan Lääne <xonproject@gmail.com>
2024-04-06 13:38:11 +0300

    Fetch Featuted Properties

--- utils/requests.js
+++ utils/requests.js
@@ -1,14 +1,19 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // Fetch all propertyes
-async function fetchProperties() {
+async function fetchProperties({ showFeatured = false } = {}) {
   try {
     // handkle case where domain is not available yet
     if (!apiDomain) {
       return [];
     }
 
-    const res = await fetch(`${apiDomain}/properties`, { cache: 'no-store' });
+    const res = await fetch(
+      `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+      {
+        cache: 'no-store',
+      }
+    );
 
     if (!res.ok) {
       throw new Error('failed to fetch data');
